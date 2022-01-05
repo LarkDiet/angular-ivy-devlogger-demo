6f01738f8ba6d488ca954fa7ff391d0f6ef9de3b
LarkDiet <nightinglarkdiet@gmail.com>
2022-01-05 18:01:22 -0500

    1 file modified

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string;
+    text: string;
+    date: any;
+  }[]
 
   constructor() { }
 
   ngOnInit() {
+    this.logs = [
+      {id:'1', text:'Generated content', date:new Date('01/05/2022 17:50:00')},
+      {id:'2', text:'Beefed it', date:new Date('01/05/2022 17:55:00')},
+      {id:'3', text:'Unbeefed it', date:new Date('01/05/2022 18:00:00')},
+    ]
   }
 
 }

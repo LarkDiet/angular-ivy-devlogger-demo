0902f4ca043bf62384fe9fa04d1be5df9f6b2928
LarkDiet <nightinglarkdiet@gmail.com>
2022-01-06 13:42:26 -0500

    3 files modified

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -41,7 +41,15 @@ export class LogService {
       if(log.id === cur.id) {
         this.logs.splice(index, 1);
       }
-    })
+    });
     this.logs.unshift(log);
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if(log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
   }
 }
